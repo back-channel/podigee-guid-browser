58c90dd24eba4c3f5a836af22df926e25bdbd4de
Philipp Bosch <hello@pb.io>
2025-05-12 23:31:04 +0200

    Add Tailwind CSS; error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { writeText } from '@tauri-apps/plugin-clipboard-manager';
 import "./App.css";
+import {twMerge} from "tailwind-merge";
 
 type Podcast = {
   id: number;
@@ -13,14 +14,25 @@ type Episode = {
   title: string;
 }
 
-function SelectedItem({ children, onReset }: { children: React.ReactNode, onReset: () => void }) {
+const SelectedItem: React.FC<React.PropsWithChildren<{ onReset: () => void }>> = ({ children, onReset })=> {
   return (
-    <div className="selected-item">
-      <div>{children}</div>
-      <span className="remove-button" aria-role="button" onClick={() => { onReset(); }}>×</span>
+    <div className="flex gap-4">
+      <div className={"flex-1 bg-neutral-500/10 rounded px-4 py-3"}>{children}</div>
+      <Button onClick={onReset}>×</Button>
     </div>
-
   );
+}
+
+const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ className, ...props }) => (
+  <input {...props} className={twMerge("bg-neutral-300 dark:bg-neutral-700 text-neutral-800 dark:text-neutral-200 px-4 py-3 rounded", className)} {...props} />
+);
+
+const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ className, children, ...props }) => (
+  <button {...props} className={twMerge("bg-neutral-500/20 hover:bg-neutral-500/30 px-4 py-3 rounded cursor-pointer", className)}>{children}</button>
+);
+
+const isError = (obj: any)=> {
+  return obj.code && obj.message;
 }
 
 function App() {
@@ -41,7 +53,6 @@ function App() {
     return response.json();
   }
 
-
   async function fetchPodcasts() {
     return apiRequest('/podcasts');
   }
@@ -61,25 +72,23 @@ function App() {
   }, [apiKey, selectedPodcast]);
 
   return (
-    <main className="container">
-      <h1>Podigee GUID Browser</h1>
-
-      <div className="row">
-      </div>
+    <main className="mx-6 my-4 flex flex-col gap-4">
+      <h1 className="text-2xl">Podigee GUID Browser</h1>
 
       {!apiKey ? (
         <form
-          className="row"
           onSubmit={async (e) => {
             e.preventDefault();
             setApiKey(e.currentTarget.apikey.value);
           }}
+          className="flex gap-4 py-1"
         >
-          <input
+          <Input
             name="apikey"
-            placeholder="Podigee API Key"
+            placeholder="Podigee API-Key"
+            className={"flex-1"}
           />
-          <button type="submit">Browse</button>
+          <Button type="submit">→</Button>
         </form>
         ) : (
           <SelectedItem
@@ -92,7 +101,11 @@ function App() {
           >API-Key: {apiKey.substring(0, 4) + '…'}</SelectedItem>
         )
       }
-
+      {isError(podcasts) && (
+        <div className={"border rounded border-red-500 bg-red-500/10 text-red-500 px-4 py-3"}>
+          Fehler: {(podcasts as any).code}<br />{(podcasts as any).message}<br />{(podcasts as any).reason}
+        </div>
+      )}
       {podcasts.length > 0 && (
         selectedPodcast ? (
           <SelectedItem
@@ -102,45 +115,44 @@ function App() {
             }}
           >Podcast: {selectedPodcast.title}</SelectedItem>
         ) : (
-          <div className="podcast-list">
+          <div className="">
             {podcasts.map((podcast) => (
-              <button className="podcast-item" key={podcast.id} onClick={() => { setSelectedPodcast(podcast); }}>
+              <Button className="w-full text-left" key={podcast.id} onClick={() => { setSelectedPodcast(podcast); }}>
                 {podcast.title}
-              </button>
+              </Button>
             ))}
           </div>
         )
       )}
 
       {episodes.length > 0 && (
-        <table>
-          <thead><tr><th>ID</th><th>Title</th><th>GUID</th></tr></thead>
+        <table className={"[&_td]:bg-neutral-500/10 [&_td]:px-4 [&_td]:py-1"}>
           <tbody>
-          {episodes.map(episode => (
-            <tr key={episode.id}>
-              <td>{episode.id}</td>
-              <td>{episode.title}</td>
-              <td>
-                <span
-                  className="copyable"
-                  onClick={async () => {
-                    await writeText(episode.guid);
-                    setToast("Copied to clipboard");
-                    window.setTimeout(() => {
-                      setToast(null);
-                    }, 3000);
-                  }}
-                >{episode.guid}</span>
-              </td>
-            </tr>
-          ))}
+            {episodes.map(episode => (
+              <tr key={episode.id} className={"[&:first-child>td:first-child]:rounded-tl [&:first-child>td:last-child]:rounded-tr [&:last-child>td:first-child]:rounded-bl [&:last-child>td:last-child]:rounded-br"}>
+                <td>{episode.id}</td>
+                <td>{episode.title} {episode.title} {episode.title}</td>
+                <td>
+                  <Button
+                    className={"w-full"}
+                    onClick={async () => {
+                      await writeText(episode.guid);
+                      setToast("GUID in die Zwischenablage kopiert");
+                      window.setTimeout(() => {
+                        setToast(null);
+                      }, 3000);
+                    }}
+                  >{episode.guid}</Button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       )}
 
-      {toast && (
-        <div className="toast">{toast}</div>
-      )}
+      <div className={twMerge("absolute bottom-4 left-4 bg-neutral-900 dark:bg-neutral-100 text-neutral-100 dark:text-neutral-900 px-4 py-3 rounded-lg shadow-lg transition",
+        !toast && "opacity-0 -translate-x-[100%]"
+      )}>{toast}</div>
     </main>
   );
 }
